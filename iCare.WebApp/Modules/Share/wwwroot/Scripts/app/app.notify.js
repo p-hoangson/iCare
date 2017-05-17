451b2a88eb633d4b5e0e4988ba7d25f66b1828c9
p-hoangson <son.phamhoang89@gmail.com>
2017-05-18 00:13:36 +0700

    them notify

--- iCare.WebApp/Modules/Share/wwwroot/Scripts/app/app.notify.js
+++ iCare.WebApp/Modules/Share/wwwroot/Scripts/app/app.notify.js
@@ -4,19 +4,46 @@
 
     App.notify = {};
 
-    var settings = {
-        //soundPath: App.baseUrl('~/Modules/Share/wwwroot/Scripts/lobibox/sounds/'),
-        msg : ''
+    var DEFAULTS = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "30000",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
     }
-   
+
     App.notify.error = function (message, options) {
-        settings.msg = message;
-        //Lobibox.notify('error', $.extend({}, settings, options));
+        toastr.options = $.extend({}, DEFAULTS, options);
+        toastr.error(message);
     };
 
     App.notify.success = function (message, options) {
-        settings.msg = message;
-        //Lobibox.notify('success', $.extend({}, settings, options));
+        toastr.options = $.extend({}, DEFAULTS, options);
+        toastr.success(message);
     };
+
+    App.notify.warning = function (message, options) {
+        toastr.options = $.extend({}, DEFAULTS, options);
+        toastr.warning(message);
+    };
+
+    App.notify.info = function (message, options) {
+        toastr.options = $.extend({}, DEFAULTS, options);
+        toastr.info(message);
+    };
+
+    App.notify.clear = function () {
+        toastr.clear()
+    }
 
 }(App || (App = {})));
